d5f5fcb052bfd988ccdd2378cbe829542439a2ab
Yunze Wan <wanyunze@gmail.com>
2024-01-31 23:10:51 +1100

    updated some file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const api = require("./api");
+const api = require("../api");
 require("dotenv").config();
 
 /**
